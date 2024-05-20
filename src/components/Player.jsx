c67c51cf04ac7a545e212a36367492eb1ee8d5e7
s.dipaolo90 <s.dipaolo90@gmail.com>
2024-05-20 21:54:37 +0000

    First functionality of the app implemented. Now the Player.jsx has been created and has been a little managed.

---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+export default function Player({name, symbol}) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = 'Edit';
+
+  if (isEditing) {
+    playerName = <input type="text" value={name} required/>;
+    btnCaption = 'Save';
+  }
+
+  return <li>
+    <span className="player">
+      {playerName}
+      <span className="player-symbol">{symbol}</span>
+    </span>
+    <button onClick={handleEditClick}>{btnCaption}</button>
+  </li>
+}
