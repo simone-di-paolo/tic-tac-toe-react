664739917a8b8ec4cf6c6ade8a663d80e03c224e
s.dipaolo90 <s.dipaolo90@gmail.com>
2024-05-21 14:10:12 +0000

    GameBoard created but it's not yet working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import Player from './components/Player'
+import GameBoard from './components/GameBoard'
 function App() {
 
   return <main>
     <div id="game-container">
       <ol id="players">
         <Player
-          name="Player 1"
+          initialName="Player 1"
           symbol="X"
         />
         <Player
-          name="Player 2"
+          initialName="Player 2"
           symbol="O"
         />
       </ol>
-      GAME BOARD
+      <GameBoard />
     </div>
 
     LOG
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   let btnCaption = 'Edit';
 
   if (isEditing) {
-    playerName = <input type="text" value={name} required/>;
+    editablePlayerName = <input type="text" value={playerName} onChange={handleChange} onChrequired/>;
     btnCaption = 'Save';
   }
 
   return <li>
     <span className="player">
-      {playerName}
+      {editablePlayerName}
       <span className="player-symbol">{symbol}</span>
     </span>
     <button onClick={handleEditClick}>{btnCaption}</button>
