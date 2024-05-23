222e9156a8aa5eb4f5ef7b17a5914dd48f300e4e
s.dipaolo90 <s.dipaolo90@gmail.com>
2024-05-23 20:51:59 +0000

    Disable the button after the player click on it

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,24 +6,13 @@ const initialGameBoard = [
 
 export default function GameBoard({ onSelectSquare, turns }) {
     let gameBoard = initialGameBoard;
-    
-    for(const turn of turns) {
+
+    for (const turn of turns) {
         const { square, player } = turn;
         const { row, col } = square;
 
         gameBoard[row][col] = player;
     }
-
-    /* const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSelectSquare(rowIndex, columnIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][columnIndex] = activePlayerSymbol;
-            return updatedBoard;
-        });
-        onSelectSquare();
-    } */
 
     return <ol id="game-board">
         {gameBoard.map((row, rowIndex) => (
@@ -31,7 +20,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                         <li key={colIndex}>
-                            <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                                {playerSymbol}
+                            </button>
                         </li>
                     ))}
                 </ol>
